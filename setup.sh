8980b3850b6ffcc67ff0ac312b9466911a7f3ce4
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-12-01 16:04:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ read -r -p "Do you want to run reflector? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     sudo pacman -Sy --needed --noconfirm reflector
     echo -e "\nIt will take time to fetch the mirrors so please wait"
-    sudo reflector --save /etc/pacman.d/mirrorlist -p https -c $(echo $LANG | awk -F _ '{print $2}') -f 10
+    sudo reflector --save /etc/pacman.d/mirrorlist -p https -c $(echo $LANG | awk -F [_,.] '{print $2}') -f 10
 fi
 
 echo ""
