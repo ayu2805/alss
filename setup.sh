8e96e301615a5e236c8686c17b115fd5aed261f7
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2025-09-01 18:40:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -129,7 +129,15 @@ curl -Ss https://gist.githubusercontent.com/ayu2805/7bae58a7e279199552f77e3ae577
 echo ""
 read -r -p "Do you want to configure git? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    ./git-config
+    read -p "Enter your Git name: " git_name
+    read -p "Enter your Git email: " git_email
+    git config --global user.name $git_name
+    git config --global user.email $git_email
+    git config --global init.defaultBranch main
+    ssh-keygen -t ed25519 -C $git_email
+    git config --global gpg.format ssh
+    git config --global user.signingkey /home/$(whoami)/.ssh/id_ed25519.pub
+    git config --global commit.gpgsign true
 fi
 
 touchpadConfig='Section "InputClass"
