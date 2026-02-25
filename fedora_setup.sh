b6c146fbd8f7c049e476387d2c3dea26aac35e68
Ayushmaan Padhi <padhiayushmaan@gmail.com>
2026-02-25 20:28:19 +0530

    Added VSCode configuration

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -77,6 +77,10 @@ configure_system() {
     sudo ufw allow SSH
     sudo ufw allow Bonjour
     echo 'PS1="\[\e[32m\][\u@\h \W]\[\e[34m\]$\[\e[0m\] "' | tee /home/$(whoami)/.bashrc > /dev/null
+
+    mkdir -p "/home/$(whoami)/.config/Code/User/"
+    curl -Ss https://gist.githubusercontent.com/ayu2805/7bae58a7e279199552f77e3ae577bd6c/raw/settings.json | \
+        tee "/home/$(whoami)/.config/Code/User/settings.json" > /dev/null
 }
 
 setup_samba() {
